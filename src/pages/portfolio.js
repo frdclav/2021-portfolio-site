a630c037c5a79810756ebe0cdcce6914ab94f5da
Francis Rodson Clavel <francisrodsonclavel@Franciss-MacBook-Pro.local>
2021-05-16 22:39:18 -0700

    add jumbotron and nav

---
+++ src/pages/portfolio.js
@@ -0,0 +1,18 @@
+// Step 1: Import your component
+import * as React from 'react'
+// import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const PortfolioPage = () => {
+  return (
+      <Layout pageTitle="Portfolio">
+            <p>Projects will go here.</p>
+   
+      </Layout>
+    
+  )
+}
+
+// Step 3: Export your component
+export default PortfolioPage
