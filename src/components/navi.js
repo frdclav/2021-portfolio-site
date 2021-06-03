117f6c127fcd18c322604cde1d48164eb48a0d9a
frdclav <frclav@gmail.com>
2021-06-03 01:43:36 -0700

    add linked in to nav

--- src/components/navi.js
+++ src/components/navi.js
@@ -1,32 +1,36 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
+import
+{
   a
 } from "./navi.module.css";
-import {
+import
+{
   Navbar,
-  Nav,Container, Jumbotron, Row, Col
-  
+  Nav, Container, Jumbotron, Row, Col
+
 } from "react-bootstrap";
 import "../styles/style.css"
 // import { Link } from 'react-router-bootstrap'
 
 
-const Navi = ({ pageTitle, children }) => {
+const Navi = ( { pageTitle, children } ) =>
+{
   return (
     <Navbar bg="light" expand="lg">
-<div><Navbar.Brand className="brand"><Link  to="/about">francis rodson d. clavel</Link></Navbar.Brand></div>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Link to="/portfolio">projects</Link>
-    </Nav>
-  </Navbar.Collapse>
+      <div><Navbar.Brand className="brand"><Link to="/about">francis rodson d. clavel</Link></Navbar.Brand></div>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to="/portfolio" style={{ padding: '5px' }}> projects </Link>
+          <Link to="https://www.linkedin.com/in/francisrodsonclavel/" style={{ padding: '5px' }}> linkedin </Link>
+        </Nav>
+      </Navbar.Collapse>
 
- 
-</Navbar>
-    
+
+    </Navbar>
+
   )
 };
 
